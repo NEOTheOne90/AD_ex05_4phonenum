4711080fbb9e1edef93835912e55707f192f1c26
NeoTheOne90 <88240177+NEOTheOne90@users.noreply.github.com>
2021-12-16 14:39:24 +0900

    Add files via upload

---
+++ src/main/java/com/example/ex05_4/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ex05_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.ex5_4_liear_layout);
+        setContentView(R.layout.ex5_4_relative_layout);
+    }
+}
